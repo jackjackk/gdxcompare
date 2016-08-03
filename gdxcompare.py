cff2df9477c21d1fed481faeda4e95f8ab7b8f89
jackjackk <jackjackk@gmail.com>
2016-08-03 13:06:29 +0100

    now supports python 3

--- gdxcompare.py
+++ gdxcompare.py
@@ -5,6 +5,7 @@ Created on Wed Mar 21 14:24:21 2012
 
 @author: Marangoni
 """
+from __future__ import print_function
 import sys, os
 from gdxpy.gdxpy import *
 import numpy as np
@@ -29,7 +30,10 @@ def add_to_doompol(new_sorted_elemlist):
         n = len(elemlist)
         is_new_list_in_elemlist = True
         for e in new_sorted_elemlist:
-            candidate_idx = np.searchsorted(elemlist,e)
+            try:
+                candidate_idx = np.searchsorted(elemlist,e)
+            except:
+                candidate_idx = -1
             if (candidate_idx == n) or (e != elemlist[candidate_idx]):
                 is_new_list_in_elemlist = False
                 break
@@ -89,126 +93,123 @@ else:
     if len(gdxNames) != len(gdxList):
         raise Exception('Please provide a rename list with the same number of elements as the gdx')
 
-fout = file(os.path.join(comparePath,'data.txt'),'w')
-fout.write('var symbList = [\n')
-
-lastnamePlaceholder = 'z'*10
-symb2gdxlist_dict = {}
-symb2desc_dict = {}
-ngdx = len(gdxList)
-grange = range(ngdx)
-for ig,gpath in enumerate(gdxList):
-    g = gdxfile(gpath)
-    for s in g.get_symbols_list():
-        if not symb_regex.match(s.name):
-            continue
-        try:
-            symb2gdxlist_dict[s.name].append(ig)
-        except:
-            symb2gdxlist_dict[s.name] = [ig,]
-            symb2desc_dict[s.name] = s.desc
-domcounter = 0
-bDone = False
-dompool = []
-for s,realgdxlist in symb2gdxlist_dict.iteritems():
-    if len(realgdxlist) < 2:
-        continue
-    try:
-        try:
-            filt = filt_dict[s]
-        except:
-            filt = None
-        svar = gload(s,[gdxList[ig] for ig in realgdxlist],reshape=False,clear=True,filt=filt,single=False,remove_underscore=False,returnfirst=True)
-    except AssertionError as e:
-        message = e.args[0]
-        print message
-        continue
-    except Exception as e:
-        traceback.print_exc()
-        #if s.name == 'Q_EN':
-        #    bDone = True
-        print 'Skipping',s
-        continue
-    try:
-        df = svar.stack().unstack(0)
-        domlist = []
-        try:
-            nx = len(df.axes[0].levels)
-        except:
-            nx = 2
-            df = pd.concat([df.stack()],keys=['only']).unstack(0)
-        for iax,ax in reversed(list(enumerate(df.axes[0].levels))):
-            tfound = True
-            for x in ax:
-                try:
-                    int(x)
-                except:
-                    tfound = False
-                    break
-            if tfound == True:
-                axsnotime = range(nx)
-                axsnotime.pop(iax)
-                newaxslist = axsnotime+[iax,]
-                #df = df.reorder_levels(axsnotime+[iax,]).sort()
-                #if iax < nx-1:
-                #    df = df.swaplevel(iax,nx-1).unstack().stack() #.sortlevel(nx-1)
-                print 'found time at iax =',iax
-                break
-        if not tfound:
-            print 'Skipping',s,'(no x-axis found)'
-            continue
-        #df2 = df.copy()
-        #df.index = pd.MultiIndex.from_product([range(len(item)) for item in df.axes[0].levels])
-        # For each row of data, of index x, build a new index where (x_i) is
-        sorted_levels_values = []
-        for i in range(nx):
-            sorted_values = np.sort(df.axes[0].levels[i].values)
-            if i==iax:
-                sorted_values = sorted_values[(sorted_values>xaxisMin) & (sorted_values<xaxisMax)]
-            domlist.append(add_to_doompol(sorted_values))
-        #df.index = pd.MultiIndex.from_tuples([tuple([np.searchsorted(sorted_levels_values[i],x[i]) for i in range(nx)]) for x in df.index])
-        data = {}
-        for k,v in df.T.iteritems():
-            if ((xaxisMin != 0) and (k[iax] < xaxisMin)) or ((xaxisMax != np.inf) and (k[iax] > xaxisMax)):
+with open(os.path.join(comparePath,'data.txt'), 'w') as fout:
+    fout.write('var symbList = [\n')
+    lastnamePlaceholder = 'z'*10
+    symb2gdxlist_dict = {}
+    symb2desc_dict = {}
+    ngdx = len(gdxList)
+    grange = list(range(ngdx))
+    for ig,gpath in enumerate(gdxList):
+        g = gdxfile(gpath)
+        for s in g.get_symbols_list():
+            if not symb_regex.match(s.name):
                 continue
-            val2write = np.zeros(ngdx)
-            #for i2read,i2write in enumerate(realgdxlist):
-            #    y = v.values[i2read]
-            #    if np.isnan(y) or (y > 1e200):
-            #        y = 0.
-            #    val2write[i2write] = y
-            for ig,g in enumerate(gdxList):
-                try:
-                    y = v[g[:-4]]
-                    if (not np.isnan(y)) and (y < 1e200):
-                        val2write[ig] = y
-                except:
-                    pass
-            data[','.join(['%d' % np.searchsorted(dompool[domlist[i]],k[i]) for i in newaxslist])] = [float('%.2e' % x) for x in list(val2write)]
-        if domcounter>0:
+            try:
+                symb2gdxlist_dict[s.name].append(ig)
+            except:
+                symb2gdxlist_dict[s.name] = [ig,]
+                symb2desc_dict[s.name] = s.desc
+    domcounter = 0
+    bDone = False
+    dompool = []
+    for s,realgdxlist in symb2gdxlist_dict.items():
+        if len(realgdxlist) < 2:
+            continue
+        try:
+            try:
+                filt = filt_dict[s]
+            except:
+                filt = None
+            svar = gload(s,[gdxList[ig] for ig in realgdxlist],reshape=False,clear=True,filt=filt,single=False,remove_underscore=False,returnfirst=True)
+        except AssertionError as e:
+            message = e.args[0]
+            print(message)
+            continue
+        except Exception as e:
+            traceback.print_exc()
+            #if s.name == 'Q_EN':
+            #    bDone = True
+            print('Skipping',s)
+            continue
+        try:
+            df = svar.stack().unstack(0)
+            domlist = []
+            try:
+                nx = len(df.axes[0].levels)
+            except:
+                nx = 2
+                df = pd.concat([df.stack()],keys=['only']).unstack(0)
+            for iax,ax in reversed(list(enumerate(df.axes[0].levels))):
+                tfound = True
+                for x in ax:
+                    try:
+                        int(x)
+                    except:
+                        tfound = False
+                        break
+                if tfound == True:
+                    axsnotime = list(range(nx))
+                    axsnotime.pop(iax)
+                    newaxslist = axsnotime+[iax,]
+                    #df = df.reorder_levels(axsnotime+[iax,]).sort()
+                    #if iax < nx-1:
+                    #    df = df.swaplevel(iax,nx-1).unstack().stack() #.sortlevel(nx-1)
+                    print('found time at iax =',iax)
+                    break
+            if not tfound:
+                print('Skipping',s,'(no x-axis found)')
+                continue
+            #df2 = df.copy()
+            #df.index = pd.MultiIndex.from_product([range(len(item)) for item in df.axes[0].levels])
+            # For each row of data, of index x, build a new index where (x_i) is
+            sorted_levels_values = []
+            for i in list(range(nx)):
+                sorted_values = np.sort(df.axes[0].levels[i].values)
+                if i==iax:
+                    sorted_values = sorted_values[(sorted_values>xaxisMin) & (sorted_values<xaxisMax)]
+                domlist.append(add_to_doompol(sorted_values))
+            #df.index = pd.MultiIndex.from_tuples([tuple([np.searchsorted(sorted_levels_values[i],x[i]) for i in range(nx)]) for x in df.index])
+            data = {}
+            for k,v in df.T.items():
+                if ((xaxisMin != 0) and (k[iax] < xaxisMin)) or ((xaxisMax != np.inf) and (k[iax] > xaxisMax)):
+                    continue
+                val2write = np.zeros(ngdx)
+                #for i2read,i2write in enumerate(realgdxlist):
+                #    y = v.values[i2read]
+                #    if np.isnan(y) or (y > 1e200):
+                #        y = 0.
+                #    val2write[i2write] = y
+                for ig,g in enumerate(gdxList):
+                    try:
+                        y = v[g[:-4]]
+                        if (not np.isnan(y)) and (y < 1e200):
+                            val2write[ig] = y
+                    except:
+                        pass
+                data[','.join(['%d' % np.searchsorted(dompool[domlist[i]],k[i]) for i in newaxslist])] = [float('%.2e' % x) for x in list(val2write)]
+            if domcounter>0:
+                fout.write(',\n')
+            fout.write('new Symb("%s","%s",%s,%s)' % (s, symb2desc_dict[s], str([domlist[i] for i in newaxslist]), str(data)))
+            domcounter += nx
+        except AssertionError as e:
+            message = e.args[0]
+            print(message)
+        except Exception as e:
+            traceback.print_exc()
+            #if s.name == 'Q_EN':
+            #    bDone = True
+            print('Skipping',s)
+
+    fout.write('];\n')
+    fout.write('var setList = [\n')
+    for idom,d in enumerate(dompool):
+        if idom > 0:
             fout.write(',\n')
-        fout.write('new Symb("%s","%s",%s,%s)' % (s, symb2desc_dict[s], str([domlist[i] for i in newaxslist]), str(data)))
-        domcounter += nx
-    except AssertionError as e:
-        message = e.args[0]
-        print message
-    except Exception as e:
-        traceback.print_exc()
-        #if s.name == 'Q_EN':
-        #    bDone = True
-        print 'Skipping',s
-
-fout.write('];\n')
-fout.write('var setList = [\n')
-for idom,d in enumerate(dompool):
-    if idom > 0:
-        fout.write(',\n')
-    fout.write('new Set("s%d",%s)' % (idom,str(list(d))))
-fout.write('];\n')
+        fout.write('new Set("s%d",%s)' % (idom,str(list(d))))
+    fout.write('];\n')
 
 
-fout.write('var series = %s;' % str([g.split('\\')[-1] for g in gdxNames]))
-
-fout.close()
+    fout.write('var series = %s;' % str([g.split('\\')[-1] for g in gdxNames]))
 
 open_file(os.path.join(comparePath,'compare.html'))
