089a2d42e6e6d25151d6e08c58b9eab8b07148be
jackjackk <jackjackk@gmail.com>
2017-02-16 12:03:16 +0000

    add validation profile

---
+++ profiles/valid.py
@@ -0,0 +1,39 @@
+{
+    'Q_EMI' : ["ch4",
+               "ch4_ffi",
+               "ch4_lu",
+               "ch4_wst",
+               "co2",
+               "co2ffi",
+               "co2lu",
+               "co2peat",
+               "f-gases",
+               "hfc",
+               "kghg",
+               "n2o",
+               "n2o_ffi",
+               "n2o_lu"],
+
+    'Q_PES' : ["coal",
+               "gas",
+               "oil",
+               "uranium",
+               "wbio"],
+
+    'Q_EN' : ["elgastr",
+              "elgeo",
+              "elhydro",
+              "elnuclear",
+              "eloil",
+              "elpb",
+              "elpc",
+              "elsolwind"],
+    
+    'Q_OUT' : ["coal",
+               "gas",
+               "oil",
+               "uranium"],
+
+    'Q' : ["y", ],
+    'l' : None
+}
