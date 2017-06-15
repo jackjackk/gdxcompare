ec5e0bd5b7efd30260cef9ed7c8f7aa16fc5864b
jackjackk <jackjackk@gmail.com>
2017-06-15 15:42:23 +0200

    clean up repo and metadata

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(name='gdxcompare',
       version='0.1.0',
-      description='Visually compare time series in GAMS GDX files',
+      description='Visually compare time series across GAMS GDX files',
       url='https://github.com/jackjackk/gdxcompare',
       author='Giacomo Marangoni',
       author_email='jackjackk@gmail.com',
       license='MIT',
       keywords='gdx gams dataviz',
       packages=['gdxcompare',],
-      install_requires=['gdxpy'],
+      install_requires=['gdxpy>=0.1.2'],
 )
