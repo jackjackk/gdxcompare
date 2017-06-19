81fa094b9892f11f86a677f4449f254e2c5dac49
jackjackk <jackjackk@gmail.com>
2017-06-19 21:31:27 +0100

    fix missing data files after pip install

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='gdxcompare',
-      version='0.1.1',
+      version='0.1.2',
       description='Visually compare time series across GAMS GDX files',
       url='https://github.com/jackjackk/gdxcompare',
       author='Giacomo Marangoni',
@@ -9,5 +9,6 @@ setup(name='gdxcompare',
       license='MIT',
       keywords='gdx gams dataviz',
       packages=['gdxcompare',],
+      package_data={'gdxcompare':['*.html', '*.js']},
       install_requires=['gdxpy>=0.1.2'],
 )
